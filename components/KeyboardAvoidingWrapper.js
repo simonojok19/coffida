5dff8925b68c7f16502c5c5b134167482d8c86ad
simonojok19 <simonojok19@gmail.com>
2021-07-11 17:41:16 +0300

    created the current wrapper

--- components/KeyboardAvoidingWrapper.js
+++ components/KeyboardAvoidingWrapper.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Keyboard, KeyboardAvoidingView, ScrollView, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 
-const KeyboardAvoidingWrapper = () => {
-  return ();
+const KeyboardAvoidingWrapper = ({children}) => {
+  return (
+    <KeyboardAvoidingView style={{flex: 1}}>
+      <ScrollView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          {children}
+        </TouchableWithoutFeedback>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 }
 
 export default KeyboardAvoidingWrapper;
