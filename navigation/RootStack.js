3079f9873815b6b40adac9415b2d8a03d298b5c3
simonojok19 <simonojok19@gmail.com>
2021-07-11 18:27:07 +0300

    finished setting up the header of the app

--- navigation/RootStack.js
+++ navigation/RootStack.js
@@ -4,13 +4,25 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/Login';
 import SignUpScreen from '../screens/Signup';
 import HomeScreen from '../screens/HomeScreen';
+import { Colors } from '../components/styles';
 
 const Stack = createStackNavigator();
 
 const RootStack = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{
+        headerStyle: {
+          backgroundColor: 'transparent'
+        },
+        headerTintColor: Colors.tertiary,
+        headerTransparent: true,
+        headerTitle: '',
+        headerLeftContainerStyle: {
+          paddingLeft: 20
+        }
+      }}
+      initialRouteName="LoginScreen">
         <Stack.Screen name="LoginScreen" component={LoginScreen}/>
         <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
