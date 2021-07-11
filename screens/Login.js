74b85bd267e0ffdd58ad7ab5faad1467042bec93
simonojok19 <simonojok19@gmail.com>
2021-07-11 13:45:34 +0300

    creating the text input component

--- screens/Login.js
+++ screens/Login.js
@@ -1,4 +1,35 @@
 import React from 'react';
+import { InnerContainer, PageLogo, PageTitle, StyledContainer, StyledFormArea, SubTitle } from '../components/styles';
+import { StatusBar } from 'expo-status-bar';
+import { Formik } from 'formik';
+import { View } from 'react-native';
 
-const Login = () => {};
+const Login = () => {
+  return (
+    <StyledContainer>
+      <StatusBar style='dark' />
+      <InnerContainer>
+        <PageLogo resizeMode='cover' source={require('./../assets/img/coffee_cup_transparent.jpg')} />
+        <PageTitle>CoffiDa</PageTitle>
+        <SubTitle>Account Login</SubTitle>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={(values) => {
+            console.log(values);
+          }}
+        >
+          {({handleChange, handleBlur, handleSubmit, values}) => <StyledFormArea>
+
+          </StyledFormArea>}
+        </Formik>
+      </InnerContainer>
+    </StyledContainer>
+  );
+};
+
+const MyTextInput = ({label, icon, ...props}) => {
+  return (<View>
+
+  </View>)
+}
 export default Login;
