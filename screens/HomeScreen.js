c7c3479425e737ee303e394f65385964f46d4355
simonojok19 <simonojok19@gmail.com>
2021-07-11 17:48:25 +0300

    added the dashboard

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import KeyboardAvoidingWrapper from '../components/KeyboardAvoidingWrapper';
+import { Text, View } from 'react-native';
+
+const HomeScreen = () => {
+  return (<KeyboardAvoidingWrapper>
+    <View>
+      <Text>Hello Dashboard</Text>
+    </View>
+  </KeyboardAvoidingWrapper>);
+}
+
+export default HomeScreen
